73d5d60aa0b535514200a1975d9a575f087c8e9d
feeta-coin <mackinjamie7@gmail.com>
2021-04-04 16:03:42 +0100

    feeta updates

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #FeetaTEST3\r");
+            Send(hSocket, "WHO #FeetaTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #Feeta00-#Feeta99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #Feeta%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #Feeta%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
